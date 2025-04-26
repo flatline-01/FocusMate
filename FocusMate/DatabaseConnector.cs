1f0a1b8b80cbca69de83e4370eb7114fd9745a26
Admin <doesithaveanysense@mailfence.com>
2025-04-26 21:35:39 +0200

    - add DatabaseConnector class - add TaskRepository class

---
+++ FocusMate/DatabaseConnector.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Npgsql;
+
+namespace FocusMate
+{
+    public class DatabaseConnector
+    {
+        private string _connectionString = 
+            "Host=localhost;Port=5432;Database=focus_mate;Username=root;Password=768867";
+
+        public NpgsqlConnection GetConnection()
+        {
+            var connection = new NpgsqlConnection(_connectionString);
+            connection.Open();
+            return connection;
+        }
+    }
+}
