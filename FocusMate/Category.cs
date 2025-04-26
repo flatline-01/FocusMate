4607fc10411996bbe9093e6f3b6aa7b486c9d470
Admin <doesithaveanysense@mailfence.com>
2025-04-26 20:56:33 +0200

    - change properties in Task and Category classes - create design for main window

---
+++ FocusMate/Category.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FocusMate
+{
+    class Category
+    {
+        public int Id;
+        public string Name;
+    }
+}
---
+++ FocusMate/Task.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FocusMate
+{
+    class Task
+    {
+        public int Id;
+        public string Title;
+        public string Description;
+        public string Category;
+        public DateTime Date;
+        public bool IsDone;
+    }
+}
