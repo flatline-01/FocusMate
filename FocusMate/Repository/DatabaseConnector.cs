cc422966290480eeacb66f7393d1ce10700b58a3
flatline-01 <doesithaveanysense@mailfence.com>
2025-06-07 12:08:44 +0200

    ... - check if database credentials aren't set - create window to save database credentials

--- FocusMate/Repository/DatabaseConnector.cs
+++ FocusMate/Repository/DatabaseConnector.cs
@@ -1,14 +1,37 @@
-﻿using System.Windows;
+﻿using System.ComponentModel;
+using System.Windows;
+using FocusMate.View;
 using Npgsql;
 
 namespace FocusMate.Repository
 {
     public class DatabaseConnector
     {
-        private static string _username = Environment.GetEnvironmentVariable("dbms_username");
-        private static string _password = Environment.GetEnvironmentVariable("dbms_password");
-        private string _connectionString = 
-            $"Host=localhost;Port=5432;Database=focus_mate;Username={_username};Password={_password}";
+        private static string _username;
+        private static string _password;
+        private string _connectionString;
+
+        public DatabaseConnector() {
+            SetVariables();
+            if (_username == null || _password == null) { 
+                var window = new DatabaseCredentialsWindow();
+                window.Closing += DatabaseCredentialsWindowClosing;
+                window.ShowDialog();
+            }
+        }
+
+        private void DatabaseCredentialsWindowClosing(object sender, CancelEventArgs e)
+        {
+            SetVariables();
+            (sender as Window).Closing -= DatabaseCredentialsWindowClosing;
+        }
+
+        private void SetVariables() {
+            _username = Environment.GetEnvironmentVariable("dbms_username", EnvironmentVariableTarget.Machine);
+            _password = Environment.GetEnvironmentVariable("dbms_password", EnvironmentVariableTarget.Machine);
+            _connectionString =
+                $"Host=localhost;Port=5432;Database=focus_mate;Username={_username};Password={_password}";
+        }
 
         public NpgsqlConnection GetConnection()
         {
---
+++ FocusMate/View/DatabaseCredentialsWindow.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+
+namespace FocusMate.View
+{
+    public partial class DatabaseCredentialsWindow : Window
+    {
+        public DatabaseCredentialsWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void SaveCredentials(object sender, RoutedEventArgs e)
+        {
+            Environment.SetEnvironmentVariable("dbms_username", UsernameTextBox.Text, EnvironmentVariableTarget.Machine);
+            Environment.SetEnvironmentVariable("dbms_password", PasswordTextBox.Password, EnvironmentVariableTarget.Machine);
+            Close();
+        }
+    }
+}
